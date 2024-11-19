6c26999116d5be48a805e2f83ae48c38224069d0
Ravi Buraga <109849640+raviburaga@users.noreply.github.com>
2024-11-19 10:21:33 +0530

    "privacy updated"

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -8,15 +8,16 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAWZc8qibm_5luDPrfzT9h9oR7JNKowE8M",
-  authDomain: "facerecognitionsystem-baa9b.firebaseapp.com",
-  databaseURL: "https://facerecognitionsystem-baa9b-default-rtdb.firebaseio.com",
-  projectId: "facerecognitionsystem-baa9b",
-  storageBucket: "facerecognitionsystem-baa9b.appspot.com",
-  messagingSenderId: "980424030441",
-  appId: "1:980424030441:web:4e1f850222ef02840d1878",
-  measurementId: "G-KQDRETK2XL"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
+;
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
